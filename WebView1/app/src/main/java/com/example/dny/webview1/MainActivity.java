8a1958ed3d268585dde63534b6ac6d71245403fe
DnyanadaArjunwadkar <dnyanada@buffalo.edu>
2016-02-10 11:23:22 -0500

    WebView1
    Simple Template of WebView1.
    This is a basic template, can be used to make complete app by adding
    more lines of code.
    Tutor:Bhairav Mehrotra

---
+++ WebView1/app/src/main/java/com/example/dny/webview1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.dny.webview1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = (WebView)findViewById(R.id.webView);
+
+        String html = "\"<html>\n" +
+                "<head></head><body><a href=\"http://www.acsu.buffalo.edu/~dnyanada/\">Visit homepage</a></body></html>";
+
+        view.loadData(html,"text/html",null);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
