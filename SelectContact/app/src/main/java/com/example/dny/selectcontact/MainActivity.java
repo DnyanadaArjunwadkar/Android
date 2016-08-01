1ed75e6e9cdf0ccb2a6e507a2f55ee3710e783fc
DnyanadaArjunwadkar <dnyanada@buffalo.edu>
2016-08-01 15:07:38 -0700

    Selects contact
    It is a simple Contact Picker app. Selects a contact and loads the
    contact name and contact number on text view.

---
+++ SelectContact/app/src/main/java/com/example/dny/selectcontact/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.dny.selectcontact;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity{
+  static final int pick=1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                Log.e("Dny -> Content_uri:", String.valueOf(ContactsContract.Contacts.CONTENT_URI));
+                startActivityForResult(i, pick);
+            }
+        });
+
+    }
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(reqCode, resultCode, data);
+        String cNumber = "";
+        String name = "";
+
+        switch (reqCode) {
+            case (pick) :
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Uri contactData = data.getData();
+                    Log.e("contactData:", String.valueOf(data.getData()));
+                    Cursor c =  managedQuery(contactData, null, null, null, null);
+
+                    if (c.moveToFirst()) {
+
+
+                        String id =c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                        Log.e("dny id:",id);
+
+                        String hasPhone =c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                   
+                        Log.e("dny hasPhhone:",hasPhone);
+                        if (hasPhone.equalsIgnoreCase("1")) {
+                            Cursor phones = getContentResolver().query(
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,
+                                    null, null);
+                            phones.moveToFirst();
+                            cNumber = phones.getString(phones.getColumnIndex("data1"));
+                            System.out.println("number is:"+cNumber);
+                        }
+                        name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+
+                    }
+                }
+                break;
+        }
+
+        TextView tv = (TextView)findViewById(R.id.tv);
+        tv.setText("You have selected :"+name + cNumber);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
